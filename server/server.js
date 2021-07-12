b1a9443c5a21cd7791316941c1a02d5fdf2aeeb8
vladimir <kobzarenkovolodymyr@gmail.com>
2021-07-12 16:09:21 +0300

    Installed mongoose. Project connected to MongoDB.

--- server/server.js
+++ server/server.js
@@ -1,8 +1,23 @@
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = require("./app");
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("DB connection successful!"));
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
