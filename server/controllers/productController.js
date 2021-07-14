e66f457a3374b558495e625bb1ac785820e4f062
vladimir <kobzarenkovolodymyr@gmail.com>
2021-07-14 22:49:13 +0300

    Created CRUD operations for 'Product'.

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,5 +1,42 @@
 const Product = require("../models/product");
 
+exports.getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+
+    res.status(200).json({
+      status: "success",
+      result: products.length,
+      data: {
+        products,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
+
+exports.getProduct = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        product,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
+
 exports.createProduct = async (req, res) => {
   try {
     const newProduct = await Product.create(req.body);
@@ -13,6 +50,43 @@ exports.createProduct = async (req, res) => {
   } catch (err) {
     res.status(400).json({
       status: "fail",
+      message: err,
+    });
+  }
+};
+
+exports.updateProduct = async (req, res) => {
+  try {
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        product,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
+
+exports.deleteProduct = async (req, res) => {
+  try {
+    await Product.findByIdAndDelete(req.params.id);
+
+    res.status(204).json({
+      status: "success",
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fali",
       message: err,
     });
   }
