46e2ffa78b321fac2838397bcda6ec1f60edbb32
vladimir <kobzarenkovolodymyr@gmail.com>
2021-07-20 21:27:39 +0300

    Added errors handlers for DB to errorController. Added catching 'unhandledRejection' and 'uncaughtException' to server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,13 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+//for sync errors
+process.on("uncaughtException", (err) => {
+  console.log("UNCAUGHT EXCEPTION!!! Shutting down...");
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 const app = require("./app");
 
 const DB = process.env.DATABASE.replace(
@@ -20,4 +27,14 @@ mongoose
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
+});
+
+//for async errors
+process.on("unhandledRejection", (err) => {
+  console.log("UNHANDLED REJECTION!!! Shutting down...");
+  console.log(err.name, err.message);
+
+  server.close(() => {
+    process.exit(1);
+  });
 });
