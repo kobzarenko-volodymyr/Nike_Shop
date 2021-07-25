a29fdb55dfa57758eb18541c433c9b01ff252864
vladimir <kobzarenkovolodymyr@gmail.com>
2021-07-25 14:59:34 +0300

    Created 'Auth Controller' and 'User Routes'. Added managing password with encryption.

--- server/app.js
+++ server/app.js
@@ -3,7 +3,8 @@ const express = require("express");
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
 
-const productRouter = require("./routes/productRouter");
+const productRouter = require("./routes/productRoutes");
+const userRouter = require("./routes/userRoutes");
 
 // Start express app
 const app = express();
@@ -13,6 +14,7 @@ app.use(express.json({ limit: "10kb" }));
 
 //ROUTES
 app.use("/api/products", productRouter);
+app.use("/api/users", userRouter);
 
 app.all("*", (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`), 404);
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -16,7 +17,7 @@ const userSchema = new mongoose.Schema({
   photo: String,
   role: {
     type: String,
-    enum: [user, admin],
+    enum: ["user", "admin"],
     default: "user",
   },
   password: {
@@ -28,7 +29,26 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, "Please, confirm your password"],
+    validate: {
+      //Works only on Create and Save!
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: "Password are not the same!",
+    },
   },
+});
+
+userSchema.pre("save", async function (next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified("password")) return next();
+
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // Delete passwordConfirm field
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model("User", userSchema);
