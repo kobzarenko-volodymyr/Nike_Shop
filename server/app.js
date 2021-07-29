16fb228703197c0a1003c50e6db040ab7165872b
vladimir <kobzarenkovolodymyr@gmail.com>
2021-07-29 22:53:15 +0300

    Added security measures: data sanitization, preventing parameter pollution, rate limiting, sending JWT via Cookie

--- server/app.js
+++ server/app.js
@@ -1,4 +1,9 @@
 const express = require("express");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -9,8 +14,33 @@ const userRouter = require("./routes/userRoutes");
 // Start express app
 const app = express();
 
+// 1) GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
+
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 300,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP, please try again in an hour!",
+});
+app.use("/api", limiter);
+
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: "10kb" }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: ["ratings", "price", "sizes", "colors"],
+  })
+);
 
 //ROUTES
 app.use("/api/products", productRouter);
